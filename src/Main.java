cd0a1309c66795a6f29024338e19229744a757cb
MarinaM <marina.miniiarova@gmail.com>
2024-05-19 17:26:38 +0700

    переименовала переменные

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        int na_schete = 100;
-        int popolnenie = 1000;
-        int summa_za_1_bonus = 100;
-        int minimalnaya_summa = 1000;
-        int nachislenie = popolnenie;
+        int currentAmount = 100; //на счете
+        int replenishment = 1000; //пополнение
+        int amountPerBonus = 100; //сумма за 1 бонус
+        int minimalAmount = 1000; //минимальная сумма
+
         int bonus = 0;
-        if (popolnenie >= minimalnaya_summa) {
-            bonus = popolnenie / summa_za_1_bonus;
+        if (replenishment >= minimalAmount) {
+            bonus = replenishment / amountPerBonus;
         }
 
-        nachislenie = nachislenie + bonus;
-        na_schete = na_schete + nachislenie;
+        int newAmount = currentAmount + replenishment + bonus;
 
-        System.out.println("На счете: " + na_schete);
+        System.out.println("На счете: " + newAmount);
         System.out.println("Сумма бонусов: " + bonus);
     }
 }
